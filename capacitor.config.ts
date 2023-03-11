82530139d0b07c8601c7229e7c806b4b0089ec63
Danilo Ley Alves <daniloleyalves1@hotmail.com>
2023-03-11 12:04:09 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fairshare_refactor',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
